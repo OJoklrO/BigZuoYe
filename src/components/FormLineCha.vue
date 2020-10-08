6fc59f086af99bb3a0f5308e8234e42f7b8104dc
gxy <xy_guo@foxmail.com>
2020-10-08 20:44:15 +0800

    format code

--- src/components/FormLineCha.vue
+++ src/components/FormLineCha.vue
@@ -1,81 +1,98 @@
 <template>
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+  <el-form :inline="true" :model="formInline" class="demo-form-inline">
     <el-form-item label="实验名称" class="room_search">
-        <el-select v-model="current_lab" placeholder="实验名称">
-          <el-option v-for="(item,index) in lab_list" :key="index" :label="item" :value="item" ></el-option>
-        </el-select>
+      <el-select v-model="current_lab" placeholder="实验名称">
+        <el-option
+          v-for="(item, index) in lab_list"
+          :key="index"
+          :label="item"
+          :value="item"
+        ></el-option>
+      </el-select>
     </el-form-item>
     <el-form-item label="实验员" class="room_search">
-        <el-select v-model="current_laber" placeholder="姓名">
-          <el-option v-for="(item,index) in laber_list" :key="index" :label="item" :value="item" ></el-option>
-        </el-select>
+      <el-select v-model="current_laber" placeholder="姓名">
+        <el-option
+          v-for="(item, index) in laber_list"
+          :key="index"
+          :label="item"
+          :value="item"
+        ></el-option>
+      </el-select>
     </el-form-item>
     <el-form-item label="实验批次" class="room_search">
-        <el-select v-model="current_batch" placeholder="批次号">
-          <el-option v-for="(item,index) in batch_list" :key="index" :label="item" :value="item" ></el-option>
-        </el-select>
+      <el-select v-model="current_batch" placeholder="批次号">
+        <el-option
+          v-for="(item, index) in batch_list"
+          :key="index"
+          :label="item"
+          :value="item"
+        ></el-option>
+      </el-select>
     </el-form-item>
     <el-form-item class="room_select_button">
-        <el-button type="primary" @click="onSubmit" icon="el-icon-search">搜索</el-button>
+      <el-button type="primary" @click="onSubmit" icon="el-icon-search"
+        >搜索</el-button
+      >
     </el-form-item>
-    </el-form>
-</template>>
+  </el-form> </template
+>>
 <script>
-  export default {
-    data() {
-      return {
-        formInline: {
-          user: '',
-          region: ''
-        },
-        current_lab:"",
-        current_laber:"",
-        current_batch:"",
-        lab_list:["物理实验","化学实验"],
-        laber_list:["任","郭","马"],
-        batch_list:["1","2","3","4"],
-        query_message:[{
-          stuId:"18130500078",
-          stuName:"任腾宇",
-          stuGrade:+100,
-          stuClass:"1803051"
+export default {
+  data() {
+    return {
+      formInline: {
+        user: "",
+        region: ""
+      },
+      current_lab: "",
+      current_laber: "",
+      current_batch: "",
+      lab_list: ["物理实验", "化学实验"],
+      laber_list: ["任", "郭", "马"],
+      batch_list: ["1", "2", "3", "4"],
+      query_message: [
+        {
+          stuId: "18130500078",
+          stuName: "任腾宇",
+          stuGrade: +100,
+          stuClass: "1803051"
         },
         {
-          stuId:"18130500026",
-          stuName:"郭向宇",
-          stuGrade:+99,
-          stuClass:"1803051"
+          stuId: "18130500026",
+          stuName: "郭向宇",
+          stuGrade: +99,
+          stuClass: "1803051"
         },
         {
-          stuId:"18130500008",
-          stuName:"马钰泓",
-          stuGrade:+98,
-          stuClass:"1803051"
+          stuId: "18130500008",
+          stuName: "马钰泓",
+          stuGrade: +98,
+          stuClass: "1803051"
         }
-        ]
-      }
-    },
-    methods: {
-      onSubmit() {
-        console.log(this.current_lab);
-        console.log(this.current_laber);
-        console.log(this.current_batch);
-        // 编写筛选函数获得query_message对象
+      ]
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.current_lab);
+      console.log(this.current_laber);
+      console.log(this.current_batch);
+      // 编写筛选函数获得query_message对象
 
-
-        var temp = []
-        temp=this.query_message
-        this.$store.commit("SetGrade", temp)
-      }
+      var temp = [];
+      temp = this.query_message;
+      this.$store.commit("SetGrade", temp);
     }
   }
+};
 </script>
 <style scoped lang="scss">
-.room_search{
+.room_search {
   margin-right: 50px;
   margin-left: 50px;
 }
-.room_capcity{
+.room_capcity {
   margin-right: 50px;
 }
 </style>
--- src/components/StudentTable.vue
+++ src/components/StudentTable.vue
@@ -1,100 +1,92 @@
 <template>
-<div>
-  <el-table
-    :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-    style="width: 100%;" max-height="420">
-    <el-table-column
-      label="学号"
-      prop="stuId">
-    </el-table-column>
-    <el-table-column
-      label="姓名"
-      prop="stuName">
-    </el-table-column>
-    <el-table-column
-      label="班级"
-      prop="stuClass">
-    </el-table-column>
-    <el-table-column
-      label="实验成绩"
-      prop="stuGrade">
-    </el-table-column>
-    <el-table-column
-      align="right">
-      <template slot="header">
-        <el-input
-          v-model="search"
-          size="mini"
-          placeholder="输入关键字搜索"/>
-      </template>
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-  <!-- 弹出框 -->
-  <el-dialog title="成绩录入" :visible.sync="dialogVisible">
-    <el-form :model="form" @submit.native.prevent>
-      <el-form-item label="分数" :label-width="formLabelWidth">
-        <el-input v-model="grade" autocomplete="off" @keyup.enter.native="submit_form()"></el-input>
-      </el-form-item>
-    </el-form>
-    <div slot="footer" class="dialog-footer">
-      <el-button @click="dialogVisible = false">取 消</el-button>
-      <el-button type="primary" @click="submit_form">确 定</el-button>
-    </div>
-  </el-dialog>
-</div>
-  
+  <div>
+    <el-table
+      :data="
+        tableData.filter(
+          data =>
+            !search || data.name.toLowerCase().includes(search.toLowerCase())
+        )
+      "
+      style="width: 100%;"
+      max-height="420"
+    >
+      <el-table-column label="学号" prop="stuId"> </el-table-column>
+      <el-table-column label="姓名" prop="stuName"> </el-table-column>
+      <el-table-column label="班级" prop="stuClass"> </el-table-column>
+      <el-table-column label="实验成绩" prop="stuGrade"> </el-table-column>
+      <el-table-column align="right">
+        <template slot="header">
+          <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
+        </template>
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+            >Edit</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 弹出框 -->
+    <el-dialog title="成绩录入" :visible.sync="dialogVisible">
+      <el-form :model="form" @submit.native.prevent>
+        <el-form-item label="分数" :label-width="formLabelWidth">
+          <el-input
+            v-model="grade"
+            autocomplete="off"
+            @keyup.enter.native="submit_form()"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submit_form">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-  import {mapState} from "vuex"
-  export default {
-    computed: {
-        ...mapState({tableData: state => state.grade.grades})
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState({ tableData: state => state.grade.grades })
+  },
+  data() {
+    return {
+      search: "",
+      grade: "",
+      item_index: -1,
+      dialogVisible: false,
+      form: {},
+      formLabelWidth: "120px"
+    };
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index);
+      console.log(row);
+      // 实现改变成绩操作
+      console.log(this.tableData[index].stuGrade);
+      this.dialogVisible = true;
+      this.item_index = index;
+      // 把grade赋值给学生
+
+      // this.tableData[index].stuGrade=this.grade;
+      // this.grade=""
     },
-    data() {
-      return {
-        search: '',
-        grade:"",
-        item_index:-1,
-        dialogVisible:false,
-        form: {
-          
-        },
-        formLabelWidth: '120px'
-      }
-    },
-    methods: {
-      handleEdit(index, row) {
-        console.log(index);
-        console.log(row);
-        // 实现改变成绩操作
-        console.log(this.tableData[index].stuGrade)
-        this.dialogVisible=true;
-        this.item_index=index
-        // 把grade赋值给学生
-        
-        // this.tableData[index].stuGrade=this.grade;
-        // this.grade=""
-      },
-      submit_form(){
-          this.tableData[this.item_index].stuGrade=this.grade;
-          this.dialogVisible = false
-          this.grade=""
-          console.log("chenggong")
-      },
-      handleDelete(index, row) {
-        console.log(index, row);
-      }
+    submit_form() {
+      this.tableData[this.item_index].stuGrade = this.grade;
+      this.dialogVisible = false;
+      this.grade = "";
+      console.log("chenggong");
     },
     handleDelete(index, row) {
       console.log(index, row);
     }
+  },
+  handleDelete(index, row) {
+    console.log(index, row);
   }
+};
 </script>
 <style lang="scss">
 .box_banner {
